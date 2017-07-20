fe1e8d85593d2adcae65a510306601d77f5ffbbd
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-20 12:55:03 +0530

    Created ClassVsInstance.java file
    First commit

---
+++ 30 Days of Code Challenges/Day 4- Class vs. Instance/ClassVsInstance.java
@@ -0,0 +1,29 @@
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+  		// Add some more code to run some checks on initialAge
+                if(initialAge > 0)
+                {
+                    age = initialAge;
+                }
+                else{
+                    age = 0;
+                    System.out.println("Age is not valid, setting age to 0.");
+                }
+	}
+
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+                if (age < 13)
+                System.out.println(/*Insert correct print statement here*/ "You are young.");
+                else if (age < 18)
+                System.out.println("You are a teenager.");
+                else
+                System.out.println("You are old.");   
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+                age = age+1;
+	}
