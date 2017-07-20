f573746944e1e3235c074070db82797dae6dff29
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-20 12:56:39 +0530

    Updated ClassVsInstance.java file
    Added the default main() method

--- 30 Days of Code Challenges/Day 4- Class vs. Instance/ClassVsInstance.java
+++ 30 Days of Code Challenges/Day 4- Class vs. Instance/ClassVsInstance.java
@@ -27,3 +27,19 @@ public class Person {
   		// Increment this person's age.
                 age = age+1;
 	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int age = sc.nextInt();
+			Person p = new Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++) {
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
