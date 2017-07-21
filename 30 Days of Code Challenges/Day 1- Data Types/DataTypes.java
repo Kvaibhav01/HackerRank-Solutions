69a47f7907b092030e26fe94bd56d9749a135ffb
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-21 12:48:29 +0530

    Updated class name
    Solution to DataTypes

--- 30 Days of Code Challenges/Day 1- Data Types/DataTypes.java
+++ 30 Days of Code Challenges/Day 1- Data Types/DataTypes.java
@@ -4,7 +4,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution {
+public class DataTypes {
 	
     public static void main(String[] args) {
         int i = 4;
