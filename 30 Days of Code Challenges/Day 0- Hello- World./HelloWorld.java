24fd01c20084a7d6d3817579d4870ff358c9a3d6
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-21 12:47:37 +0530

    Updated class name
    Solution to HelloWorld

--- 30 Days of Code Challenges/Day 0- Hello- World./HelloWorld.java
+++ 30 Days of Code Challenges/Day 0- Hello- World./HelloWorld.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
-public class Solution {
+public class HelloWorld {
 	public static void main(String[] args) {
   
     // Create a Scanner object to read input from stdin.
