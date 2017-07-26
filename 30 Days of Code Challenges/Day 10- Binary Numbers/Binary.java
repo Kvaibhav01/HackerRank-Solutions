ef4500dbc94a35423bca57d1e64d1c82b2da74d9
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-26 20:10:56 +0530

    Created Binary.java
    First commit

---
+++ 30 Days of Code Challenges/Day 10- Binary Numbers/Binary.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Binary {
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        
+        int sum = 0, max = 0;
+        
+        while (n > 0) 
+        {
+            if (n % 2 == 1) 
+            {
+                sum++;
+                if (sum > max) 
+                {
+                    max = sum;
+                }
+            } 
+            else 
+            {
+                sum = 0;
+            }
+            
+            n = n / 2;
+        }
+        
+        System.out.println(max);
+    }
+}
