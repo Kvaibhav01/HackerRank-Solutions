cff3f088644ee3f47d540962ed7815ee1a3ea49f
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-29 19:59:18 +0530

    Created Abstract.java
    First commit

---
+++ 30 Days of Code Challenges/Day 13- Abstract Classes/Abstract.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+    
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+    
+    abstract void display();
+}
+
+//Make an abstract class named MyBook
+class MyBook extends Book 
+{
+    private int price;
+
+    MyBook(String t, String a, int p) 
+    {
+        super(t, a);
+        price = p;
+    }
+
+    @Override
+    void display() 
+    {
+        System.out.println("Title: " + title + "\nAuthor: " + author + "\nPrice: " + price);
+    }
+}
+
+public class Abstract 
+{
+   
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
