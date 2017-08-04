9e3a331d22a81d2e4b28e5f3147e3f3176ae093c
Vaibhav Khulbe <kvaibhav01@live.in>
2017-08-04 20:22:27 +0530

    Updated Interfaces.java
    Improved the if statement

--- 30 Days of Code Challenges/Day 19- Interfaces/Interfaces.java
+++ 30 Days of Code Challenges/Day 19- Interfaces/Interfaces.java
@@ -13,7 +13,7 @@ class Calculator implements AdvancedArithmetic
         int sum = 0;
         for (int i = 1; i <= n; i++) 
         {
-            if (n % i == 0) 
+            if ((n % i) == 0) 
             sum=sum+i;
         }
         return sum;
