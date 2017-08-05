e9b80e55fb265e7fbdc98a84b8eac0628c028207
Vaibhav Khulbe <kvaibhav01@live.in>
2017-08-05 19:41:39 +0530

    Created Soring.java
    First commit

---
+++ 30 Days of Code Challenges/Day 20- Sorting/Sorting.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Sorting 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) 
+        {
+            arr[i] = in.nextInt();
+        }
+
+
+        int numOfSwaps = 0;
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n - 1; j++) 
+            {
+                if (arr[j] > arr[j + 1]) 
+                {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    numSwaps++;
+                }
+            }
+
+            if (numSwaps == 0) 
+            {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in " + numSwaps + " swaps.");
+        System.out.println("First Element: " + arr[0]);
+        System.out.println("Last Element: " + arr[arr.length - 1]);
+    }
+}
