e8c6cf75df6cce5e7351a5bfba4da795cc200c46
Vaibhav Khulbe <kvaibhav01@live.in>
2017-09-16 12:46:47 +0530

    Create Solution.java
    First commit

---
+++ Cracking the Coding Interview/Trees- Is This a Binary Search Tree/Solution.java
@@ -0,0 +1,21 @@
+boolean checkBST(Node root) {
+  return BST(root, -1, -1);
+}
+
+boolean BST(Node root, int valLow, int valHigh) 
+{
+  if(root == null) 
+  {
+    return true;
+  } 
+  else if (valLow >= 0 && root.data <= valLow) 
+  {
+    return false;
+  } 
+  else if (valHigh >= 0 && root.data >= valHigh) 
+  {
+    return false;
+  } 
+
+  return (BST(root.left,valLow,root.data) && isBST(root.right,root.data,valHigh));
+}
