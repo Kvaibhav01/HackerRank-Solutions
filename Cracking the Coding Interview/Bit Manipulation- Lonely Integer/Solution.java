1450b5581d973f05c3e425de3dc28585889d6397
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-04 20:30:21 +0530

    Fixed identation

--- Cracking the Coding Interview/Bit Manipulation- Lonely Integer/Solution.java
+++ Cracking the Coding Interview/Bit Manipulation- Lonely Integer/Solution.java
@@ -6,29 +6,37 @@ import java.util.regex.*;
 
 public class Solution {
 
-  public static int lonelyInteger(int a[], int counts[]) {
-    for( int i = 0; i < a.length; i++) {
+  public static int lonelyInteger(int a[], int counts[]) 
+  {
+    for( int i = 0; i < a.length; i++) 
+    {
       int value = a[i];
       counts[value]++;
     }
 
-    for( int i = 0; i < counts.length; i++) {
-      if (counts[i] == 1) {
+    for( int i = 0; i < counts.length; i++) 
+    {
+      if (counts[i] == 1) 
+      {
         return i;
       }
     }
     return -1;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
     int a[] = new int[n];
     int maxInt = 0;
-    for(int a_i=0; a_i < n; a_i++){
+    
+    for(int a_i=0; a_i < n; a_i++)
+    {
       int nextInt = in.nextInt();
       a[a_i] = nextInt;
-      if (nextInt > maxInt) {
+      if (nextInt > maxInt) 
+      {
         maxInt = nextInt;
       }
     }
