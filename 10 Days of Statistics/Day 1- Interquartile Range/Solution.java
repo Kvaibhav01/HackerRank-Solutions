1ee1bfcff62e5ee958ac47be0181925c9b19f70c
Vaibhav Khulbe <kvaibhav01@live.in>
2017-10-18 19:07:55 +0530

    Created Day 1 second solution file
    First commit

---
+++ 10 Days of Statistics/Day 1- Interquartile Range/Solution.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        
+        int [] elt = new int[size];
+        int [] freq = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            elt[i] = scan.nextInt();
+        }
+        
+        int numOfElements = 0;
+        for (int i = 0; i < size; i++) {
+            freq[i] = scan.nextInt();
+            numOfElements += freq[i];
+        }
+        scan.close();
+        
+        
+        int [] data = new int[numOfElements]; 
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < freq[i]; j++) {
+                data[index] = elt[i];
+                index++;
+            }
+        }
+        Arrays.sort(data); 
+        
+        
+        double q1 = findMedian(data, 0, data.length / 2 - 1);
+        double q3 = findMedian(data, (data.length + 1) / 2, data.length - 1);
+        System.out.println(q3 - q1); 
+    }
+    
+    
+    private static double findMedian(int [] array, int start, int end) {
+        if ((end - start) % 2 == 0) { 
+            return (array[(end + start) / 2]);
+        } else { 
+            int value1 = array[(end + start) / 2];
+            int value2 = array[(end + start) / 2 + 1];
+            return (value1 + value2) / 2.0;
+        }
+    }
+}
