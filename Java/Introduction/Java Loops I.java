a2cf20cf5cfae88b523f7703cbe660e6b868a807
Vaibhav Khulbe <kvaibhav01@live.in>
2017-11-09 19:40:30 +0530

    Create Java Loops I.java
    First commit.

---
+++ Java/Introduction/Java Loops I.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int multiplier = scan.nextInt();
+        scan.close();
+        for (int i = 1; i <= 10; i++) {
+            System.out.format("%d x %d = %d%n", multiplier, i , i * multiplier);
+        }
+    }
+}
