aa263b374de59da2e40b895042b418ddbd2e4628
Vaibhav Khulbe <kvaibhav01@live.in>
2017-11-09 19:47:12 +0530

    Create Java End-of-file.java
    First commit.

---
+++ Java/Introduction/Java End-of-file.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 1;
+        while (scan.hasNextLine()) { 
+            System.out.println(i + " " + scan.nextLine());
+            i++;
+        }
+        scan.close();
+    }
+}
