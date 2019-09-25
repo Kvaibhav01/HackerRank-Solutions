2e98a6d3fbe00ae68b93c67c798219802589fb81
Liel Fridman <lielft@gmail.com>
2019-09-25 21:34:55 +0300

    Added complete code and also a solution for the left rotation

--- Data Structures/Arrays/Arrays - DS/solution.js
+++ Data Structures/Arrays/Arrays - DS/solution.js
@@ -1,3 +1,30 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
 // Helper function: just swap two places in the array
 function swap(array, a, b) {
     let tmp = array[a];
@@ -10,4 +37,18 @@ function reverseArray(a) {
         swap(a, i, a.length - 1 - i);
     }
     return a;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
 }
