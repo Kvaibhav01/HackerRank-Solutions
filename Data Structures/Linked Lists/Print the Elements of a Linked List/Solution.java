1ba0cb128da745e4dd4afb30d9266c98e23ab3de
samuelribeiroc <samuel.costa@ccc.ufcg.edu.br>
2019-10-02 21:36:10 -0300

    Rename path and changes in code

--- Data Structures/Linked Lists/Print the Elements of a Linked List/Solution.java
+++ Data Structures/Linked Lists/Print the Elements of a Linked List/Solution.java
@@ -51,12 +51,15 @@ public class Solution {
      * }
      *
      */
+	
     static void printLinkedList(SinglyLinkedListNode head) {
         SinglyLinkedListNode aux = head;
+
         while (aux.next != null) {
             System.out.println(aux.data);
             aux = aux.next;
         }
+
         System.out.println(aux.data);
     }
 
