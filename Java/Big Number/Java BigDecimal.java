b655cb74880fbc2a8b4d4d57666282e0b5b74b41
Restyled.io <commits@restyled.io>
2020-10-04 18:18:27 +0000

    Restyled by clang-format

--- Java/Big Number/Java BigDecimal.java
+++ Java/Big Number/Java BigDecimal.java
@@ -1,24 +1,24 @@
 import java.math.BigDecimal;
 import java.util.*;
-class Solution{
-    public static void main(String []args){
+class Solution {
+  public static void main(String[] args) {
 
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n+2];
-        for(int i=0;i<n;i++){
-            s[i]=sc.next();
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    String[] s = new String[n + 2];
+    for (int i = 0; i < n; i++) {
+      s[i] = sc.next();
+    }
+    sc.close();
+
+    for (int i = 0; i < n - 1; i++) {
+      for (int j = 0; j < n - 1 - i; j++) {
+        BigDecimal a = new BigDecimal(s[j]);
+        BigDecimal b = new BigDecimal(s[j + 1]);
+        if (a.compareTo(b) < 0) {
+          String temp = s[j];
+          s[j] = s[j + 1];
+          s[j + 1] = temp;
         }
-        sc.close();
-
- for(int i=0;i<n-1;i++){
-           for(int j=0;j<n-1-i;j++){
-               BigDecimal a=new BigDecimal(s[j]);
-               BigDecimal b=new BigDecimal(s[j+1]);
-               if(a.compareTo(b)<0){
-                   String temp=s[j];
-                   s[j]=s[j+1];
-                   s[j+1]=temp;
-               }
-           }
-       }
+      }
+    }
